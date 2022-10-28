1b1c5815c3c477f27a04e69f83309ff78be27b5e
gaothing <744989745@qq.com>
2022-10-28 11:33:23 +0800

    优化

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
+
 import prompts from 'prompts';
 import fs from 'fs';
 import download from "./utils/download.js";
+import chalk from 'chalk';// 用于改变文字颜色
 const root = process.cwd();
 //查看版本号
 const inputList = [
@@ -9,15 +11,39 @@ const inputList = [
     type: 'text',
     name: 'projectName',
     message:"请输入项目名称"
+  },
+  {
+    type: 'select',
+    name: 'projectType',
+    message: '请选择项目类型',
+    choices: [
+      { title: 'admin', value: 'http://gitlab.wangxiao.cn:9090/YGZT/wangxiao-open-web.git' },
+      { title: 'h5', value: 'http://gitlab.wangxiao.cn:9090/YGZT/wangxiao-h5-cli.git' },
+      { title: 'doc', value: 'http://gitlab.wangxiao.cn:9090/gaoyanpeng/wangxiao-project-doc.git' },
+    ],
   },{
     type: 'text',
-    name: 'author',
-    message:"请输入作者"
+    name: 'username',
+    message:"请输入用户名"
+  }, {
+    type: 'password',
+    name: 'password',
+    message:"请输入密码"
   }
 ];
 (async () => {
-  const { projectName } = await prompts(inputList);
-  fs.mkdirSync(root + '/' + projectName);
-  await download('direct:https://github.com/gaothing/gitdemo.git', projectName, {});
-  console.log(`🎈🎈🎈 文件夹构建成功`)
+  const { projectName, username, password,projectType } = await prompts(inputList);
+  if (username === 'zdwx' && password === 'zdwx123') {
+    const path = root + '/' + projectName;
+    fs.mkdirSync(path);
+    await download(`direct:${projectType}`, projectName,path);
+    console.log(`🎈🎈🎈 项目构建完成
+        ${chalk.green('cd'+' '+projectName)}
+        ${chalk.green('npm install')}
+        ${chalk.green('npm run dev')}
+    `)
+  } else {
+    console.log(chalk.red(`error`),'用户名或密码错误')
+  }
+
 })();
--- utils/download.js
+++ utils/download.js
@@ -1,18 +1,19 @@
 import download from 'download-git-repo';
 import ora from 'ora';
+import fs from 'fs';
 
-// const symbols = require('log-symbols');  // 用于输出图标
-import chalk from 'chalk';
-// const chalk = require('chalk'); // 用于改变文字颜色
-export default function (remote, name, option) {
+import chalk from 'chalk';// 用于改变文字颜色
+export default function (remote, name, path) {
     const downSpinner = ora('正在下载模板...').start();
     return new Promise((resolve, reject) => {
         download(remote, name, {clone:true}, err => {
             if (err) {
                 downSpinner.fail();
                 console.log(chalk.red(err));
+                fs.rmdirSync(path);
                 reject(err);
                 return;
+
             };
             downSpinner.succeed(chalk.green('模板下载成功！'));
             resolve();
